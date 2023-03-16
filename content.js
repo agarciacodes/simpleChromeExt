a5f995bdd7d1117f0ab18d0b7ff979ba551d1d90
agarciacodes <70783190+agarciacodes@users.noreply.github.com>
2023-03-15 22:42:05 -0400

    Three files necessary for chrome ext
    Before packing into chrome, place in a folder. Use Manifest Version 3 when creating a chrome extension.

---
+++ content.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById("goToFB").addEventListener("click", function() {
+                window.open("https://www.facebook.com/theshimmychanga", "_blank");
+            })
+}
